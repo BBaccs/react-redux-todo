2d42adebc4c3c4caafba172329f60cc495d701f8
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2024-07-15 12:56:55 -0400

    remove li

--- src/Todo.js
+++ src/Todo.js
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Todo = ({ task, removeTodo, editTodo }) => (
-    <li>
-        {task}
+    <>
+        <span>{task}</span>
         <button onClick={removeTodo}>X</button>
         <button onClick={editTodo}>Edit</button>
-    </li>
+    </>
 );
 
 Todo.propTypes = {
