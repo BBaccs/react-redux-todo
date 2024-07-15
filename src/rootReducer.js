b155dc642003bbf0af21987a6cde31450451cd6e
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2024-07-15 12:46:24 -0400

    Update rootReducer.js

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,4 +1,4 @@
-import { ADD_TODO, REMOVE_TODO, EDIT_TODO } from './actionCreators'
+import { ADD_TODO, REMOVE_TODO, EDIT_TODO } from './actionCreators';
 
 const initialState = {
     todos: [],
@@ -7,23 +7,24 @@ const initialState = {
 
 export default function rootReducer(state = initialState, action) {
     switch (action.type) {
-        case ADD_TODO:
-            // debugger;
-            const newState = { ...state };
-            newState.id++;
+        case ADD_TODO: {
+            const newTodo = { task: action.task, id: state.id + 1 };
             return {
-                ...newState,
-                todos: [...newState.todos, { task: action.task, id: newState.id }]
+                ...state,
+                todos: [...state.todos, newTodo],
+                id: state.id + 1
             };
+        }
         case EDIT_TODO: {
             const editTodos = state.todos.map(val => 
                 val.id === action.id ? { ...val, task: action.task } : val
             );
             return { ...state, todos: editTodos };
         }
-        case REMOVE_TODO:
+        case REMOVE_TODO: {
             const updatedTodos = state.todos.filter(val => val.id !== action.id);
             return { ...state, todos: updatedTodos };
+        }
         default:
             return state;
     }
